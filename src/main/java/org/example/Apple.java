cd97dbb24b635ef1bd5b1c8788449ab8dcd9799f
Egorov Ilya <i.egorov@astondevs.ru>
2025-01-19 17:55:26 +0300

    Initial commit

---
+++ src/main/java/org/example/Apple.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Apple {
+
+    public int weight;
+    public String color;
+
+    public Apple(int weight, String color) {
+        this.weight = weight;
+        this.color = color;
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Apple apple1 = new Apple(100, "Blue");
+
+
+    }
+}
